5a27c5fc6570af9f119a4eac4acf769ec26b490d
Pavani-Kamboji <pavanikamboji123@gmail.com>
2024-09-08 22:02:57 +0530

    git code to depict conflicts

--- EvenOrOdd.c
+++ EvenOrOdd.c
@@ -3,16 +3,16 @@ int main() {
     int num,number;
     printf("enter a number to identify if entered number is an even or odd number: ");
     scanf("%d",&num);
-    if(num<0) {
+    if(num>0) {
         printf("please enter positive numbers");
         return 0;
-    }
     number=num%2;
     if(number==0) {
         printf("entered number is even number");
     }
     else {
         printf("entered number is odd number");
+    }
     }
     return 0;
 }
