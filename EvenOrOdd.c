55629ad3300fc1af63854db720c567c6e7169b11
Pavani-Kamboji <pavanikamboji123@gmail.com>
2024-09-08 22:12:50 +0530

    Code to depict conflicts

--- EvenOrOdd.c
+++ EvenOrOdd.c
@@ -3,6 +3,10 @@ int main() {
     int num,number;
     printf("enter a number to identify if it's an even or odd number: ");
     scanf("%d",&num);
+    if(num<0) {
+        printf("please enter positive numbers");
+        return 0;
+    }
     number=num%2;
     if(number==0) {
         printf("entered number is even number");
